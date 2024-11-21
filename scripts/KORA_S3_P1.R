1ff30b8fcbd424b4c01926fcbc9a4949e849b145
FlorianSchw <Florian.Schwarz@dife.de>
2024-11-21 15:20:40 +0100

    changes to P1 documents and scripts

--- scripts/KORA_S3_P1.R
+++ scripts/KORA_S3_P1.R
@@ -17,19 +17,25 @@ library(car)
 #### Step 0: Name of the study
 dataset_name <- "KORA_S3_P1"
 
-#### Step 1: Import overall DataSchema (need to update path)
-dataschema_1 <- tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P1.xlsx"), sheet = 1))
-dataschema_2 <- tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P1.xlsx"), sheet = 2))
+#### Step 1: Import overall DataSchema
+dataschema_1 <- tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P1.xlsx"), sheet = 1))
+dataschema_2 <- tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P1.xlsx"), sheet = 2))
 
 dataschema <- list(Variables = dataschema_1,
                    Categories = dataschema_2)
 
-#### Step 2: Import Datasets (need to update path)
-input_dataset <- read_csv(here::here("data", paste0("DATA_", dataset_name, ".csv")))
+#### Step 2: Import Datasets 
+#### Import check provided in case the csv file is in German style (delim = ";", dec.point = ",")
 
-#### Step 3: Import Data Dictionaries of the study (need to update path)
-dd_var <- tibble(readxl::read_excel(here::here("rmonize/data_dictionary", paste0("DD_",dataset_name, ".xlsx")), sheet = 1))
-dd_cat <- tibble(readxl::read_excel(here::here("rmonize/data_dictionary/", paste0("DD_",dataset_name, ".xlsx")), sheet = 2))
+input_dataset <- readr::read_csv(here::here("data", paste0("DATA_", dataset_name, ".csv")))
+
+if(dim(input_dataset)[2] == 1){
+  input_dataset <- read.csv(here::here("data", paste0("DATA_", dataset_name, ".csv")), sep = ";", dec = ",")
+}
+
+#### Step 3: Import Data Dictionaries of the study
+dd_var <- tibble::tibble(readxl::read_excel(here::here("rmonize/data_dictionary", paste0("DD_",dataset_name, ".xlsx")), sheet = 1))
+dd_cat <- tibble::tibble(readxl::read_excel(here::here("rmonize/data_dictionary/", paste0("DD_",dataset_name, ".xlsx")), sheet = 2))
 
 dd <- list(Variables = dd_var,
            Categories = dd_cat)
@@ -37,30 +43,26 @@ dd <- list(Variables = dd_var,
 #### Step 4: Import prepared Data Processing Elements (DPE)
 data_proc_elem <- readxl::read_excel(here::here("rmonize/data_proc_elem", paste0("DPE_",dataset_name, ".xlsx")), sheet = 1)
 
-#### Step 5: Combine input datasets and data dictionaries in a dossier
-dataset <- data_dict_apply(
+#### Step 5: Combine input datasets and data dictionaries into a dossier
+dataset <- madshapR::data_dict_apply(
   dataset = input_dataset,
   data_dict = dd)
 
-dossier <- dossier_create(dataset_list = list(
+dossier <- madshapR::dossier_create(dataset_list = list(
   dataset))
 
 #### Step 6: Create the harmonized dossier using the dossier, overall dataschema and DPE's
-harmonized_dossier <- harmo_process(
+harmonized_dossier <- Rmonize::harmo_process(
   dossier, 
   dataschema, 
   data_proc_elem)
 
-#### Step 7: Create Reports on the harmonized dossiers
+#### Step 7: Evaluate and summarize a harmonized dossier containing multiple harmonized datasets
+harmonized_dossier_evaluation <- Rmonize::harmonized_dossier_evaluate(harmonized_dossier)
+harmonized_dossier_summary <- Rmonize::harmonized_dossier_summarize(harmonized_dossier)
 
-# Evaluate and summarize a harmonized dossier containing multiple harmonized datasets.
 
-harmonized_dossier_evaluation <- harmonized_dossier_evaluate(harmonized_dossier)
-harmonized_dossier_summary <- harmonized_dossier_summarize(harmonized_dossier)
-
-# place your harmonized dossier in a folder. This folder name is mandatory, and 
-# must not previously exist. include with paste0 the datetime of report and study name
-
+#### Step 8: Create Reports on the harmonized dossiers; Folder name will include study name, date and time
 system_time <- Sys.time()
 system_name <- stringr::str_replace_all(string = system_time,
                                         pattern = "[-:]",
@@ -71,16 +73,15 @@ system_name <- stringr::str_replace_all(string = system_name,
 system_name <- stringr::str_split(string = system_name,pattern = "[.]")[[1]][1]
 
 bookdown_path <- here::here("output/rmonize_report/", paste0(dataset_name, "_", system_name))
-harmonized_dossier_visualize(harmonized_dossier, bookdown_path)
+Rmonize::harmonized_dossier_visualize(harmonized_dossier, bookdown_path)
 
 # Open the visual report in a browser.
-bookdown_open(bookdown_path)
+fabR::bookdown_open(bookdown_path)
 
-#### Step 8: Extract and save harmonized data into a pre-set folder
+#### Step 9: Extract and save harmonized data into a pre-set folder
+harmonized_dataset <- Rmonize::pooled_harmonized_dataset_create(harmonized_dossier)
 
-
-# Extracting the harmonized dataset
-harmonized_dataset <- pooled_harmonized_dataset_create(harmonized_dossier)
-
-# Saving the data as a csv file for upload into Opal/DataSHIELD
-write.csv(harmonized_dataset, file = here::here(paste0("output/harmonised_dataset/", dataset_name, "_harmonized.csv")))
+readr::write_delim(x = harmonized_dataset, 
+                   file = here::here(paste0("output/harmonised_dataset/", dataset_name, "_harmonized.csv")),
+                   delim = ",",
+                   na = "")
