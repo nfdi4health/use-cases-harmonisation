2f5b7faa3b06b79da8c4c1b0b9652924bade4b2f
Sofia Siampani <sofiamaria.siampani@mdc-berlin.de>
2024-12-08 19:48:18 +0100

    testing workflow

--- tests/assign_variables.R
+++ tests/assign_variables.R
@@ -1,51 +1,59 @@
-dataschema_1 <- tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P1.xlsx"), sheet = 1))
-dataschema_2 <- tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P1.xlsx"), sheet = 2))
-
-dataschema_p1 <- list(Variables = dataschema_1,
-                   Categories = dataschema_2)
-dataschema_1 <- tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P2.xlsx"), sheet = 1))
-dataschema_2 <- tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P2.xlsx"), sheet = 2))
-
-dataschema_p2 <- list(Variables = dataschema_1,
-                   Categories = dataschema_2)
-intersect <- intersect(dataschema_p1$Variables$name, dataschema_p2$Variables$name)
-union <- union(dataschema_p1$Variables$name, dataschema_p2$Variables$name)
-
-franzi_vars <-union[9:127]
-tracy_vars <-union[c(1:4,6, 128, 129, 131:139, 144:185)] 
-ines_vars <- union[c(5, 7, 8, 130, 140:143, 186:212)]
-
-P1_P2_intersect <- cbind(intersect, dataschema_p1$Variables$index[dataschema_p1$Variables$name %in% intersect], dataschema_p2$Variables$index[dataschema_p2$Variables$name %in% intersect])
-colnames(P1_P2_intersect) <- c("name", "index_P1", "index_P2")
-
-P1_P2_union <- data.frame(
-  name = union,
-  index_P1 = match(union, dataschema_p1$Variables$name), # Index of variables in dfA (NA if not present)
-  index_P2 = match(union, dataschema_p2$Variables$name)  # Index of variables in dfB (NA if not present)
-)
-
-dpe_P1 <- readxl::read_excel(here::here("rmonize/data_proc_elem/DPE_KARMEN_P1.xlsx"), sheet = 1)
-dpe_P2 <- readxl::read_excel(here::here("rmonize/data_proc_elem/DPE_KARMEN_P2.xlsx"), sheet = 1)
-
-assign_and_compare <- function(dfA, dfB, tracy_vars, ines_vars, franzi_vars) {
+assign_and_compare <- function(studyname) {
+  dataschema_p1 <- list(Variables =  tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P1.xlsx"), sheet = 1)),
+                        Categories = tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P1.xlsx"), sheet = 2)))
+  dataschema_p2 <- list(Variables =  tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P2.xlsx"), sheet = 1)),
+                        Categories = tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P2.xlsx"), sheet = 2)))
+  
+  union <- union(dataschema_p1$Variables$name, dataschema_p2$Variables$name)
+  
+  franzi_vars <-union[9:127]
+  tracy_vars <-union[c(1:4,6, 128, 129, 131:139, 144:185)] 
+  ines_vars <- union[c(5, 7, 8, 130, 140:143, 186:212)]
+  
+  dpe_p1 <- readxl::read_excel(here::here("rmonize/data_proc_elem", paste0("DPE_", studyname, "_P1.xlsx")), sheet = 1)
+  dpe_p2 <- readxl::read_excel(here::here("rmonize/data_proc_elem", paste0("DPE_", studyname, "_P2.xlsx")), sheet = 1)
   # Initialize output lists for each person
   tracy <- list()
   ines <- list()
   franzi <- list()
   
   # Get the list of all unique variables
-  all_vars <- union(dfA$dataschema_variable, dfB$dataschema_variable)
+  all_vars <- union(dpe_p1$dataschema_variable, dpe_p2$dataschema_variable)
   
   for (var in all_vars) {
-    # Get indices of the variable in dfA and dfB
-    idxA <- which(dfA$dataschema_variable == var)
-    idxB <- which(dfB$dataschema_variable == var)
+    # Validation logic
+    if (var %in% dataschema_p1$Variables$name && var %in% dataschema_p2$Variables$name) {
+      # Check against both dpe_p1 and dpe_p2
+      if (!var %in% dpe_p1$dataschema_variable) {
+        warning(sprintf("Warning: 'var' (%s) is in 'dataschema_p1' but does not match 'dpe_p1$dataschema_variable' (%s).", var, dpe_p1$dataschema_variable))
+      }
+      if (!var %in% dpe_p2$dataschema_variable) {
+        warning(sprintf("Warning: 'var' (%s) is in 'dataschema_p2' but does not match 'dpe_p2$dataschema_variable' (%s).", var, dpe_p2$dataschema_variable))
+      }
+    } else if (var %in% dataschema_p1$Variables$name) {
+      # Check against dpe_p1 only
+      if (!var %in% dpe_p1$dataschema_variable) {
+        warning(sprintf("Warning: 'var' (%s) is in 'dataschema_p1' but does not match 'dpe_p1$dataschema_variable' (%s).", var, dpe_p1$dataschema_variable))
+      }
+    } else if (var %in% dataschema_p2$Variables$name) {
+      # Check against dpe_p2 only
+      if (!var %in% dpe_p2$dataschema_variable) {
+        warning(sprintf("Warning: 'var' (%s) is in 'dataschema_p2' but does not match 'dpe_p2$dataschema_variable' (%s).", var, dpe_p2$dataschema_variable))
+      }
+    } else {
+      # Handle case where 'var' is in neither dataschema
+      warning(sprintf("Warning: 'var' (%s) is not found in 'dataschema_p1$Variables$name' or 'dataschema_p2$Variables$name'.", var))
+    }
+    
+    # Get indices of the variable in dpe_p1 and dpe_p2
+    idxA <- which(dpe_p1$dataschema_variable == var)
+    idxB <- which(dpe_p2$dataschema_variable == var)
    
     # Determine the source of the variable and compare if necessary
     if (length(idxA) > 0 && length(idxB) > 0) { # Exists in both
-      # Get rows from dfA and dfB
-      valueA <- dfA[idxA, ]
-      valueB <- dfB[idxB, ]
+      # Get rows from dpe_p1 and dpe_p2
+      valueA <- dpe_p1[idxA, ]
+      valueB <- dpe_p2[idxB, ]
       
       # Exclude the $input_dataset column for comparison
       compareA <- valueA[, !(colnames(valueA) %in% "input_dataset"), drop = FALSE]
@@ -56,17 +64,17 @@ assign_and_compare <- function(dfA, dfB, tracy_vars, ines_vars, franzi_vars) {
         value <- valueA
       } else {
         value <- valueA
-        value$`Mlstr_harmo::status` <- "ERROR" # Indicate mismatch
+        value$`Mlstr_harmo::comment` <- "ERROR" # Indicate mismatch
       }
-    } else if (length(idxA) > 0) { # Exists only in dfA
-      value <- dfA[idxA, ]
-    } else if (length(idxB) > 0) { # Exists only in dfB
-      value <- dfB[idxB, ]
+    } else if (length(idxA) > 0) { # Exists only in dpe_p1
+      value <- dpe_p1[idxA, ]
+    } else if (length(idxB) > 0) { # Exists only in dpe_p2
+      value <- dpe_p2[idxB, ]
     } else {
       next # Skip if it doesn't exist in either
     }
-    # Set $input_dataset to "KARMEN_PX"
-    value$input_dataset <- "KARMEN_PX"
+    # Set $input_dataset to ""
+    value$input_dataset <- ""
     
     # Assign the variable to the appropriate person
     if (var %in% tracy_vars) {
@@ -83,10 +91,8 @@ assign_and_compare <- function(dfA, dfB, tracy_vars, ines_vars, franzi_vars) {
     Ines = ines,
     Franzi = franzi
   )
-  
+  writexl::write_xlsx(result$Tracy, paste0("Tracy/DPE_", studyname, "_TRACY.xlsx"))
+  writexl::write_xlsx(result$Ines, paste0("Ines/DPE_", studyname, "_INES.xlsx"))
+  writexl::write_xlsx(result$Franzi, paste0("Franzi/DPE_", studyname, "_FRANZI.xlsx"))
   return(result)
 }
-
-writexl::write_xlsx(result$Tracy, "tests/DPE_KARMEN_TRACY.xlsx")
-writexl::write_xlsx(result$Ines, "tests/DPE_KARMEN_INES.xlsx")
-writexl::write_xlsx(result$Franzi, "tests/DPE_KARMEN_FRANZI.xlsx")
