695a9899b3dc8a621d9c2bd03c00dc07f92f2170
Sofia Siampani <sofiamaria.siampani@mdc-berlin.de>
2025-01-10 15:52:41 +0100

    tests for checking consistency between dataschema and dpe and common variables between dpe p1/p2

---
+++ tests/dpe_consistency.R
@@ -0,0 +1,30 @@
+studyname = "LISA"
+
+dataschema1 <- list(Variables = tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P1.xlsx"), sheet = 1)),
+                    Categories = tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P1.xlsx"), sheet = 2)))
+
+dataschema2 <- list(Variables = tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P2.xlsx"), sheet = 1)),
+                    Categories = tibble::tibble(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P2.xlsx"), sheet = 2)))
+
+# DPEs need to be loaded
+data_proc_elem_P1 <- readxl::read_excel(here::here("rmonize/data_proc_elem", paste0("DPE_",studyname, "_P1", ".xlsx")), sheet = 1)
+data_proc_elem_P2 <- readxl::read_excel(here::here("rmonize/data_proc_elem", paste0("DPE_",studyname,"_P2", ".xlsx")), sheet = 1)
+
+# Comparing dpes with dataschemas
+data_proc_elem_P1[,1:4] == dataschema1$Variables[,1:4]
+data_proc_elem_P2[,1:4] == dataschema2$Variables[,1:4]
+
+
+# Comparing common variables in P1 and P2
+common_variables <- intersect(data_proc_elem_P1$dataschema_variable, data_proc_elem_P2$dataschema_variable)
+
+comparison <- data_proc_elem_P1 %>% filter(dataschema_variable %in% common_variables)  %>% arrange(dataschema_variable)==
+  data_proc_elem_P2 %>% filter(dataschema_variable %in% common_variables) %>% arrange(dataschema_variable)
+
+comparison[,2] <- sort(common_variables)
+
+# Checking individual discrepancies
+variable = "VEGETABLES_02"
+
+data_proc_elem_P1%>% filter(dataschema_variable == variable)%>%select(input_variables)
+data_proc_elem_P2%>% filter(dataschema_variable == variable)%>%select(input_variables)
