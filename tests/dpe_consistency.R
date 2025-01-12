d9702dd5b0d1e9a3f6d3b7948a7b55ff40f2eb92
Sofia Siampani <sofiamaria.siampani@mdc-berlin.de>
2025-01-12 20:19:39 +0100

    remove whitespace test

--- tests/dpe_consistency.R
+++ tests/dpe_consistency.R
@@ -10,6 +10,18 @@ dataschema2 <- list(Variables = tibble::tibble(readxl::read_excel(here::here("rm
 data_proc_elem_P1 <- readxl::read_excel(here::here("rmonize/data_proc_elem", paste0("DPE_",studyname, "_P1", ".xlsx")), sheet = 1)
 data_proc_elem_P2 <- readxl::read_excel(here::here("rmonize/data_proc_elem", paste0("DPE_",studyname,"_P2", ".xlsx")), sheet = 1)
 
+# data_proc_elem_P1 <- data_proc_elem_P1 %>%
+#   mutate(across(
+#     c(2,5:8),
+#     ~ gsub("\\s+", "", .)
+#   ))
+# 
+# data_proc_elem_P2 <- data_proc_elem_P2 %>%
+#   mutate(across(
+#     c(2,5:8),
+#     ~ gsub("\\s+", "", .)
+#   ))
+
 # Comparing dpes with dataschemas
 data_proc_elem_P1[,1:4] == dataschema1$Variables[,1:4]
 data_proc_elem_P2[,1:4] == dataschema2$Variables[,1:4]
@@ -24,7 +36,7 @@ comparison <- data_proc_elem_P1 %>% filter(dataschema_variable %in% common_varia
 comparison[,2] <- sort(common_variables)
 
 # Checking individual discrepancies
-variable = "VEGETABLES_02"
+variable = "CAKES_12"
 
 data_proc_elem_P1%>% filter(dataschema_variable == variable)%>%select(input_variables)
 data_proc_elem_P2%>% filter(dataschema_variable == variable)%>%select(input_variables)
