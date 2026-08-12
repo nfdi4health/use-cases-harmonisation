8ce1f99cdf70a924ea185f0b71d609a5596e6034
FlorianSchw <Florian.Schwarz@dife.de>
2026-08-12 23:10:08 +0200

    docs: re-structuring the mica files for MDC server

--- utils/mica/mica_transformation.R
+++ utils/mica/mica_transformation.R
@@ -6,51 +6,27 @@ dataset_name <- list.files(here::here("scripts"))
 for(i in 1:length(dataset_name)){
   
   
-  #### part 1: the combined dataschema + dpe file
-  
+  #### Part 1: DPE
   
   datasets_variables <- as_tibble(dataset_name[i]) |> 
     mutate(value = stringr::str_split_i(string = value,
                                         pattern = "\\.R",
                                         i = 1)) |> 
-    mutate(Project1 = stringr::str_detect(string = value,
-                                          pattern = "_P1")) |> 
-    mutate(dataschema = case_when(Project1 == TRUE ~ list(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P1.xlsx"), sheet = 1)),
-                                  Project1 == FALSE ~ list(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P2.xlsx"), sheet = 1)))) |>
-    select(-Project1) |> 
     rowwise() |> 
     mutate(dpe = list(readxl::read_excel(here::here("rmonize/data_proc_elem/", paste0("DPE_", value ,".xlsx")), sheet = 1))) |> 
     ungroup() |> 
-    unnest(cols = c(dataschema, dpe), names_sep = "_") |> 
+    unnest(cols = c(dpe)) |> 
     rename(table = value,
-           index = dataschema_index,
-           valueType = dataschema_valueType,
-           label = dataschema_label,
-           name = dataschema_name) |> 
-    select(-c(dpe_index,
-              dpe_dataschema_variable,
-              dpe_label,
-              dpe_valueType)) |> 
-    mutate(entityType = "Participant",
-           referencedEntityType = "",
-           mimeType = "",
-           repeatable = "0",
-           occurrenceGroup = "") |> 
+           name = dataschema_variable,
+           "Mlstr_harmo::dataschema_variable" = input_variables) |> 
+    relocate(index) |> 
     mutate(unit = stringr::str_squish(string = stringr::str_remove(string = stringr::str_split_i(string = label,
                                                                                                  pattern = "\\[",
                                                                                                  i = 2),
                                                                    pattern = "\\]"))) |> 
-    mutate(across(everything(), ~ifelse(is.na(.), "", .))) |> 
-    rename_with(.cols = all_of(starts_with("dpe_")), .fn = ~ stringr::str_replace(string = .x,
-                                                                                  pattern = "dpe_",
-                                                                                  replacement = "")) |> 
-    mutate(input_dataset = stringr::str_split_i(string = input_dataset,
-                                                pattern = "_",
-                                                i = 1)) |> 
-    rename("Mlstr_harmo::dataschema_variable" = input_variables,
-           "Mlstr_harmo::dataschema_table" = input_dataset)
-  
-  
+    relocate(unit, .before = valueType) |> 
+    select(-input_dataset) |> 
+    mutate(across(everything(), ~ifelse(is.na(.), "", .)))
   
   
   
@@ -65,9 +41,7 @@ for(i in 1:length(dataset_name)){
                                   Project1 == FALSE ~ list(readxl::read_excel(here::here("rmonize/data_schema/", "Dataschema_P2.xlsx"), sheet = 2)))) |> 
     select(-Project1) |> 
     unnest(cols = c(dataschema)) |> 
-    rename(table = value) |> 
-    mutate(missing = "0") |> #### this feels wrong !!!, 0 could mean FALSE, and we have in RKI and NAKO datasets missing = TRUE !!!
-    mutate(code = "")
+    rename(table = value) 
   
   
   
@@ -75,7 +49,10 @@ for(i in 1:length(dataset_name)){
                      Categories = datasets_categories)
   
   
-  writexl::write_xlsx(dataschema, here::here("mica/", paste0("NFDI4Health.", dataset_name[i], "-dictionary.xlsx")))
+  writexl::write_xlsx(dataschema, here::here("mica/dpe/", paste0(stringr::str_split_i(string = dataset_name[i],
+                                                                                  pattern = "\\.R",
+                                                                                  i = 1), 
+                                                             ".xlsx")))
   
   
   
@@ -89,7 +66,7 @@ for(i in 1:length(dataset_name)){
     unnest(cols = c(dd)) |> 
     rename(table = value) |> 
     relocate(index)
-    
+  
   
   dd_categories <- as_tibble(dataset_name[i]) |> 
     mutate(value = stringr::str_split_i(string = value,
@@ -98,18 +75,50 @@ for(i in 1:length(dataset_name)){
     mutate(dd = list(readxl::read_excel(here::here("rmonize/data_dictionary/", paste0("DD_", value ,".xlsx")), sheet = 2))) |> 
     unnest(cols = c(dd)) |> 
     rename(table = value) 
-   
+  
   
   dd <- list(Variables = dd_variables,
-                     Categories = dd_categories)
+             Categories = dd_categories)
   
   
-  writexl::write_xlsx(dd, here::here("mica/", paste0("DD_", dataset_name[i], "_1.xlsx")))
+  writexl::write_xlsx(dd, here::here("mica/dd/", paste0("DD_", 
+                                                        stringr::str_split_i(string = dataset_name[i],
+                                                                             pattern = "\\.R",
+                                                                             i = 1), 
+                                                        ".xlsx")))
+  
+  
+}
+
+
+pilot_project <- c("P1", "P2")
+
+for (i in 1:length(pilot_project)){
+  
+  
+  dataschema_harmonization_variable <- as_tibble(readxl::read_excel(here::here("rmonize/data_schema/", paste0("Dataschema_", pilot_project[i],".xlsx")), sheet = 1)) |> 
+    mutate(unit = stringr::str_squish(string = stringr::str_remove(string = stringr::str_split_i(string = label,
+                                                                                                 pattern = "\\[",
+                                                                                                 i = 2),
+                                                                   pattern = "\\]"))) |> 
+    mutate(across(everything(), ~ifelse(is.na(.), "", .))) |> 
+    relocate(unit, .before = valueType) |> 
+    mutate(table = "Harmonization") |> 
+    relocate(table, .before = name)
   
   
   
+  dataschema_harmonization_category <- as_tibble(readxl::read_excel(here::here("rmonize/data_schema/", paste0("Dataschema_", pilot_project[i],".xlsx")), sheet = 2)) |> 
+    mutate(across(everything(), ~ifelse(is.na(.), "", .))) |> 
+    mutate(table = "Harmonization") |> 
+    relocate(table, .before = variable)
   
   
   
+  dataschema <- list(Variables = dataschema_harmonization_variable,
+                     Categories = dataschema_harmonization_category)
+  
+  
+  writexl::write_xlsx(dataschema, here::here("mica/ds/", paste0("Dataschema_", pilot_project[i], ".xlsx")))
   
 }
